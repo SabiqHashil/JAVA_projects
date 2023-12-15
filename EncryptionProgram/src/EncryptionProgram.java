c5c4401d9a56e6f0e02bed65d2f9e74644b61c93
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-12-16 00:06:23 +0530

    updated

--- EncryptionProgram/src/EncryptionProgram.java
+++ EncryptionProgram/src/EncryptionProgram.java
@@ -24,6 +24,7 @@ public class EncryptionProgram {
 		askQuestion();
 		
 	}
+	
 	private void askQuestion(){
 		
 		while(true) {
@@ -54,19 +55,89 @@ public class EncryptionProgram {
 		}
 		
 	}
+	
 	private void newKey() {
 		
+		character = ' '+1;
+		list.clear();
+		shuffledList.clear();
+		
+		for(int i=32; i<127; i++) {
+			list.add(Character.valueOf(character));
+			character++;
+		}
+		
+		shuffledList = new ArrayList(list);
+		Collections.shuffle(shuffledList);
+		System.out.println("*A new key has been generated*");
+		
 	}
+	
 	private void getKey() {
 		
+		System.out.println("Key: ");
+		for(Character x : list) {
+			System.out.print(x);
+		}
+		System.out.println();
+		for(Character x : shuffledList) {
+			System.out.print(x);
+		}
+		System.out.println();
+		
 	}
+	
 	private void encrypt() {
 		
+		System.out.println("Enter a message to be encrypted: ");
+		String message = scanner.nextLine();
+		
+		letters = message.toCharArray();
+		
+		for(int i = 0; i < letters.length; i++) {
+			
+			for(int j =0; j < list.size(); j++) {
+				if(letters[i]==list.get(j)) {
+					letters[i]=shuffledList.get(j);
+					break;
+				}
+			}
+		}
+		System.out.println("Encrypted: ");
+		for(char x : letters) {
+			System.out.print(x);
+		}
+		System.out.println();
 	}
+	
 	private void decrypt() {
 		
+		System.out.println("Enter a message to be decrypted: ");
+		String message = scanner.nextLine();
+		
+		letters = message.toCharArray();
+		
+		for(int i = 0; i < letters.length; i++) {
+			
+			for(int j =0; j < shuffledList.size(); j++) {
+				if(letters[i]==shuffledList.get(j)) {
+					letters[i]=list.get(j);
+					break;
+				}
+			}
+		}
+		System.out.println("Decrypted: ");
+		for(char x : letters) {
+			System.out.print(x);
+		}
+		System.out.println();
 	}
+		
+	
 	private void quit() {
+		
+		System.out.println("Thank you, have a nice day bro!");
+		System.exit(0);
 		
 	}
 	
