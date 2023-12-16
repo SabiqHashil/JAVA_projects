8eb5cf2f2683b5709e6c6983aecfca066b05f6a9
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-12-16 23:10:00 +0530

    updated

--- TextEditorApp/src/TextEditor.java
+++ TextEditorApp/src/TextEditor.java
@@ -1,20 +1,84 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class TextEditor extends JFrame implements ActionListener {
 	
+	
+	JTextArea textArea;
+	JScrollPane scrollPane;
+	JLabel fontLabel;
+	JSpinner fontSizeSpinner;
+	JButton fontColorButton;
+	
+	
+	
 	TextEditor(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Bro text Editor");
+		this.setTitle("SBQ text Editor");
 		this.setSize(500, 500);
 		this.setLayout(new FlowLayout());
+		this.setLocationRelativeTo(null);
+	
+		textArea = new JTextArea();
+		textArea.setPreferredSize(new Dimension(450, 450));
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setFont(new Font("Arial", Font.PLAIN,20));
+		
+		scrollPane = new JScrollPane(textArea);
+		scrollPane.setPreferredSize(new Dimension(450, 450));
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		fontLabel =  new JLabel("Font: ");
+		
+		fontSizeSpinner = new JSpinner();
+		fontSizeSpinner.setPreferredSize(new Dimension(50,25));
+		fontSizeSpinner.setValue(20);
+		fontSizeSpinner.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				textArea.setFont(new Font(textArea.getFont().getFamily(),Font.PLAIN,(int)fontSizeSpinner.getValue()));
+			}
+			
+		});
+		
+		fontColorButton = new JButton("Color");
+		fontColorButton.addActionListener(this);
+		
+		this.add(fontLabel);
+		this.add(fontSizeSpinner);
+		this.add(fontColorButton);
+		this.add(scrollPane);
 		this.setVisible(true);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==fontColorButton) {
+			JColorChooser colorChooser = new JColorChooser();
+			
+			Color color = colorChooser.showDialog(null, "Choose a color", Color.black);
+			
+			textArea.setForeground(color);
+			
+			
+		}
 		
 	}
 
