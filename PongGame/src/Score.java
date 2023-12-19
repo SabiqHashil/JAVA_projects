4e3ef0395dcb92ff62e226231cd072db3b2d46a9
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-12-19 22:05:03 +0530

    updated

--- PongGame/src/Score.java
+++ PongGame/src/Score.java
@@ -1,4 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
 public class Score {
-
+	
+	Score(){
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
 }
