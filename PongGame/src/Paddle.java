5046b3d19282eac217da24c8e72017a2a32291b5
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-12-19 23:00:12 +0530

    updated

--- PongGame/src/Paddle.java
+++ PongGame/src/Paddle.java
@@ -5,12 +5,27 @@ import javax.swing.*;
 
 public class Paddle extends Rectangle{
 	
-	Paddle(){
-		
+	int id;
+	int yVelocity;
+	int speed = 10;
+	
+	Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
+		super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+		this.id=id;
 	}
 	
 	public void keyPressed(KeyEvent e) {
-		
+		switch(id) {
+		case 1:
+			if(e.getKeyCode()==KeyEvent.VK_W) {
+				setYDirection(-speed);
+				move();
+			}
+			if(e.getKeyCode()==KeyEvent.VK_S) {
+				setYDirection(speed);
+				move();
+			}
+		}
 	}
 	
 	public void keyReleased(KeyEvent e) {
@@ -26,6 +41,12 @@ public class Paddle extends Rectangle{
 	}
 	
 	public void draw(Graphics g) {
+		if(id==1) {
+			g.setColor(Color.blue);
+		}else {
+			g.setColor(Color.red);
+		}
+		g.fillRect(x, y, width, height);
 		
 	}
 	
