ec93c78852562cc4fce692385b2e719019a2dfee
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-12-22 22:56:55 +0530

    updated

--- PongGame/src/GamePanel.java
+++ PongGame/src/GamePanel.java
@@ -35,7 +35,8 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void newBall() {
-		
+//		random = new Random();
+		ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),(GAME_HEIGHT/2)-(BALL_DIAMETER/2),BALL_DIAMETER,BALL_DIAMETER);
 	}
 	
 	public void newPaddles() {
@@ -54,14 +55,38 @@ public class GamePanel extends JPanel implements Runnable{
 	public void draw(Graphics g) {
 		paddle1.draw(g);
 		paddle2.draw(g);
+		ball.draw(g);
 	}
 	
 	public void move() {
-		
+		paddle1.move();
+		paddle2.move();
+		ball.move();
 	}
 	
 	public void checkCollision() {
 		
+//		bounce ball off top & bottom window edges
+		if(ball.y <= 0) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		if(ball.y >= GAME_HEIGHT-BALL_DIAMETER) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		
+		// stops paddles at window edges
+		if(paddle1.y <= 0) {
+			paddle1.y = 0;
+		}
+		if(paddle1.y >= (GAME_HEIGHT-PADDLE_HEIGHT)) {
+			paddle1.y = GAME_HEIGHT-PADDLE_HEIGHT;
+		}
+		if(paddle2.y <= 0) {
+			paddle2.y = 0;
+		}
+		if(paddle2.y >= (GAME_HEIGHT-PADDLE_HEIGHT)) {
+			paddle2.y = GAME_HEIGHT-PADDLE_HEIGHT;
+		}
 	}
 	
 	public void run() {
